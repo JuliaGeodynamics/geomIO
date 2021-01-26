4b5e83924deb18741dd275da59e319202884387e
lumoser <lumoser@students.uni-mainz.de>
2021-01-26 14:20:57 +0100

    txt_file

--- src/geomio.py
+++ src/geomio.py
@@ -10,5 +10,16 @@ import sys, os
 
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPDF, renderPM
+import svgpathtools as pt
 
-drawing = svg2rlg("vector.svg")
+
+os.chdir("..")
+#drawing = svg2rlg("input/vector.svg")
+#x = drawing.getBounds()
+f = open("input/vector.svg")
+p = f.readlines()
+x,y,z  = pt.svg2paths2("input/vector.svg")
+x_1 = x[0]
+b = x_1.point(0)
+
+Bez= p[56]
