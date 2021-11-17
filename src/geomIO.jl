ad58d1ef3e0e50b3a1d8251b0ad0529203c21be6
Boris Kaus kaus@uni-mainz.de <boris@gaia>
2021-11-17 10:56:23 +0100

    adding info on how to install PyCall/conda

--- src/geomIO.jl
+++ src/geomIO.jl
@@ -6,12 +6,12 @@ const pygeomio = PyNULL()
 
 function __init__()
 
-  # Load the required python packages (installs them in the local conda installation if required)
+  # Required python packages (installs them in the local conda installation if possible)
   module_name   = ["svgpathtools","numpy","stl","matplotlib","scipy","ipdb","vtk"]
   packages      = ["svgpathtools","numpy","numpy-stl","matplotlib","scipy","ipdb","vtk"]
 
   if PyCall.conda
-    println("Employing the build-in julia conda python distribution")
+    println("Employing the build-in julia conda python distribution for geomIO")
     
     # Import packages
     chn           =   "conda-forge"
@@ -20,17 +20,22 @@ function __init__()
     end
    
   else  
-    println("Employing the build-in python distribution, located in $(PyCall.libpython).")
+    println("Employing an existing python distribution for geomIO, located in $(PyCall.libpython).")
     println("Ensure that the following packages are installed there: $(packages)")
+    println("Alternatively, you can install manually instruct PyCall to use the build-in conda distribution with:")
+    println("julia> using Pkg")
+    println("julia> ENV[\"PYTHON\"] =\"\"")
+    println("julia> Pkg.add(\"PyCall\")")
+    println("Restart julia after this")
     
     # Import packages
     for i=1:length(module_name)
-      pyimport(module_name[i], packages[i],  chn)
+      pyimport(module_name[i], packages[i])
     end
 
   end
 
-  pushfirst!(PyVector(pyimport("sys")["path"]), (@__DIR__)*"/python")  # relative to /src of geomIO
+  pushfirst!(PyVector(pyimport("sys")."path"), (@__DIR__)*"/python")  # relative to /src of geomIO
   copy!(pygeomio, pyimport("geomio"))
 
 end
