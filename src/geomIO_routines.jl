6c6e7f74e5b1e0250d8eabae5297796904a617d8
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2021-11-17 18:38:47 +0100

    add support for stl meshes

--- src/geomIO_routines.jl
+++ src/geomIO_routines.jl
@@ -1,6 +1,10 @@
 # This provides interfaces to the various python routines
 
-export geomioFront
+using MeshIO, GeometryBasics    # STL in julia
+
+export save, load # load and save STL meshes
+
+export geomioFront, getBounds, getLayers, triSurfOpen
 
 """
     geomioFront(inFile::String, outFile::String, numInterLayers::Int64, nPrec::Int64, Volume::Bool)
@@ -30,4 +34,68 @@ function geomioFront(inFile::String, outFile::String, numInterLayers::Int64, nPr
     println("Wrote ./output/$(outFile)")
     
     return nothing 
+end
+
+
+"""
+    getBounds(inFile::String, numInterLayers::Int64, nPrec::Int64)
+
+Prints the boundaries of the mesh or volume
+"""
+function getBounds(inFile::String, numInterLayers::Int64, nPrec::Int64)
+    
+    return pygeomio.getBounds(inFile, numInterLayers, nPrec)
+end
+
+"""
+    l,nl = getLayers(inFile::String)
+
+Returns the number of layers `nl` in the file as well as the path's per layer, `l`    
+"""
+function getLayers(inFile::String)
+    l,nl = pygeomio.getLayers(inFile)
+    return l, nl
+end
+
+
+"""
+    tri = triSurfOpen(inFile::String, numInterLayers::Int64, nPrec::Int64)
+
+Reads the SVG file `inFile` and reconstructs a triangular surface from it. The surface is *not* closed.  
+`tri` is a triangular `stl` mesh that is consistent with `MeshIO`
+
+"""
+function triSurfOpen(inFile::String, numInterLayers::Int64, nPrec::Int64)
+
+    mesh, triangles = pygeomio.triSurfOpen(inFile,numInterLayers,nPrec)
+    
+    # reconstruct the normals to the triangles
+    lc          = pygeomio.getCarthesian(inFile,numInterLayers,nPrec)
+    normalspy   = pygeomio.triNormals(triangles, lc)
+    
+    # Load points & vertexes
+    points      = connect(lc,Point{3})
+
+    # Reinterpolate the mesh 
+    triangle_count = size(triangles,1)
+    faces           = Array{GLTriangleFace}(undef, triangle_count)
+    vertices        = Array{Point3f}(undef, triangle_count * 3)
+    normals         = Array{Vec3f}(undef, triangle_count * 3)
+    for i=1:triangle_count
+        j=i-1;
+        faces[j+1] = GLTriangleFace(j * 3 + 1, j * 3 + 2, j * 3 + 3)
+
+        normals[j*3+1] = Vec3f(normalspy[i,:])
+        normals[j*3+2] = normals[j*3+1] # hurts, but we need per vertex normals
+        normals[j*3+3] = normals[j*3+1]
+
+        vertices[j*3+1] = points[triangles[i,1] .+ 1]
+        vertices[j*3+2] = points[triangles[i,2] .+ 1]
+        vertices[j*3+3] = points[triangles[i,3] .+ 1]
+    end
+
+    # Create triangular mesh (STL format)
+    tri  = Mesh(meta(vertices; normals=normals), faces)
+
+    return tri
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using Test, geomIO
 
 
+
+
+
 inFile          = "./input/over.svg"
 outFile         = "folds2.stl"
 Volume          = false
@@ -9,3 +12,10 @@ nPrec           = 40;
 geomioFront(inFile,outFile, numInterLayers, nPrec, Volume)
 @test isfile("./output/$outFile")
 
+# number of layers
+l,nl = getLayers(inFile)
+@test nl==3
+
+# construct triangular stl mesh from *.,svg file & read it into julia
+tri = triSurfOpen(inFile, numInterLayers, nPrec)
+@test  tri[200][1][1] ≈ 127.50775f0
