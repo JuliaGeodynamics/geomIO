4e901f74ae3ceac953960643fe366d0e4b74649f
Boris Kaus kaus@uni-mainz.de <boris@gaia>
2021-11-17 10:23:57 +0100

    updating & giving more info in case conda fails

--- src/geomIO.jl
+++ src/geomIO.jl
@@ -1,38 +1,42 @@
 module geomIO
 
-# Force using conda python installation
-ENV["PYTHON"]=""
-using Pkg
-Pkg.add("PyCall")
-
+# Try forcing using conda python installation
 using  Conda, PyCall
-export pygeomio
-
-@show PyCall.pyversion, PyCall.libpython, PyCall.conda
-
 const pygeomio = PyNULL()
 
 function __init__()
 
-    # Load the required python packages (installs them in the local conda installation if required)
-    chn     =   "conda-forge"
-    Conda.add("svgpathtools",   :my_env, channel=chn)
-    Conda.add("numpy",          :my_env, channel=chn)
-    Conda.add("numpy-stl",      :my_env, channel=chn)
-    Conda.add("matplotlib",     :my_env, channel=chn)
-    Conda.add("scipy",          :my_env, channel=chn)
-    Conda.add("ipdb",           :my_env, channel=chn)
-    Conda.add("vtk",            :my_env, channel=chn)
+  # Load the required python packages (installs them in the local conda installation if required)
+  module_name   = ["svgpathtools","numpy","stl","matplotlib","scipy","ipdb","vtk"]
+  packages      = ["svgpathtools","numpy","numpy-stl","matplotlib","scipy","ipdb","vtk"]
 
-  #  pushfirst!(PyVector(pyimport("sys")["path"]), (PROJECT_ROOT)*"/python")
-    pushfirst!(PyVector(pyimport("sys")["path"]), (@__DIR__)*"/python")  # relative to /src of geomIO
-    copy!(pygeomio, pyimport("geomio"))
+  if PyCall.conda
+    println("Employing the build-in julia conda python distribution")
+    
+    # Import packages
+    chn           =   "conda-forge"
+    for i=1:length(module_name)
+      pyimport_conda(module_name[i], packages[i],  chn)
+    end
+   
+  else  
+    println("Employing the build-in python distribution, located in $(PyCall.libpython).")
+    println("Ensure that the following packages are installed there: $(packages)")
+    
+    # Import packages
+    for i=1:length(module_name)
+      pyimport(module_name[i], packages[i],  chn)
+    end
+
+  end
+
+  pushfirst!(PyVector(pyimport("sys")["path"]), (@__DIR__)*"/python")  # relative to /src of geomIO
+  copy!(pygeomio, pyimport("geomio"))
 
 end
+export pygeomio
 
 include("./geomIO_routines.jl")
-
-
 
 
 end # module
